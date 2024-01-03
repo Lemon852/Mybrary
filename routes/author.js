f0ef16c25b53ee81f46ba33d14aa9d3e6cceff99
Lemon <leedhakal85@gmail.com>
2024-01-03 13:07:06 -0500

    This is third commit after adding search feature

--- routes/author.js
+++ routes/author.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const Author = require("../models/author");
+const author = require("../models/author");
 
 //all the author router
-router.get("/", (req, res) => {
-  res.render("authors/index");
+router.get("/", async (req, res) => {
+
+
+  //req.query is used for get request while
+  // req.body is used for post request.
+  //RegExp(regular expression will help us to search for items without completing the word/text)
+  let searchOptions = {}
+  if(req.query.name != null && req.query.name !==''){
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+
+  try{
+    const authors = await Author.find(searchOptions)
+    res.render("authors/index.ejs", {
+      authors:authors,
+      searchOptions: req.query
+    })
+  }catch{
+    res.redirect('/')
+  }
 });
 
 // all the new author router
@@ -13,8 +32,21 @@ router.get("/new", (req, res) => {
 });
 
 //create new author router
-router.post("/", (req, res) => {
-  res.send("Create");
-});
+router.post('/', async (req, res) => {
+  const author = new Author
+  ({
+    name: req.body.name, //we wamt to make sure which item to send
+
+  })
+  try{
+    const newAuthor = await author.save()
+    res.redirect("authors");
+  }catch{
+      res.render("authors/new",{
+        author: author,
+        errorMessage: 'Error creating Author!'
+      })
+    }
+})
 
 module.exports = router;
